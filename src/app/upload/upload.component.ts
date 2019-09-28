421365a09b1c9c9159a1a621d05425cf11ef6f92
Meyling <meyling85@gmail.com>
2019-09-27 22:33:49 -0500

    Refactorizar

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -12,30 +12,37 @@ export class UploadComponent implements OnInit {
   public progress: number;
   public equinos=[];
   public bovinos=[];
+  public error : string;
   constructor(private uploadService: UploadService) { }
 
   ngOnInit() {
   }
 
   upload(files) {
+    //Si el archivo es vacio generamos error
     if (files.length === 0)
       return;
 
+    //Iniciamos formulario
     const formData = new FormData();
 
+    //Obtenemos archivo del formulario
     for (let file of files)
       formData.append(file.name, file);
 
+    //Llamamos servicio encargado de subir inventario
     this.uploadService.uploadInventory(formData).subscribe(event => {
       if (event.type === HttpEventType.UploadProgress)
         this.progress = Math.round(100 * event.loaded / event.total);
       else if (event.type === HttpEventType.Response){
+        //Si la peticion es exitosa asignamos valores
         const data=JSON.parse(JSON.stringify( event.body));
         this.equinos =  data.equinos;
         this.bovinos =  data.bovinos;
+        this.error="";
       }
         
-    });
+    },error=>this.error="Error Generando inventario");
   }
 
 }
